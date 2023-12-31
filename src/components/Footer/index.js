21c879d6ae97fc72f89ccbaa3c9d07bc1c890147
Daniel Xu <xudh47@gmail.com>
2023-12-31 08:07:46 -0800

    Updated Footer to use primarily MUI instead of CSS to be more media responsive

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,24 +1,55 @@
 import React from "react";
 import { Box, Grid, Typography, Link } from "@mui/material";
-import "./Footer.css"; // Ensure this import is correct
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <Box component="footer" className="footer-container" sx={{ width: "100%" }}>
-      <Grid container alignItems="center" className="footer-grid">
-        <Grid item xs={12} className="footer-content">
-          {/* Empty Box used to push content to the right */}
-          <Box flexGrow={1}></Box>
-          <Box className="footer-logo-box">
-            <div className="footer-logo"></div> {/* Logo */}
+    <Box
+      component="footer"
+      sx={{
+        position: "relative",
+        padding: "20px",
+        backgroundColor: "black",
+        color: "white",
+        width: "100%",
+        "&::before": {
+          content: '""',
+          position: "absolute",
+          top: 0,
+          left: "50%",
+          width: "80%",
+          height: "2px",
+          backgroundColor: "white",
+          transform: "translateX(-50%)",
+        },
+      }}
+    >
+      <Grid
+        container
+        alignItems="center"
+        sx={{ display: "flex", justifyContent: "flex-end" }}
+      >
+        <Grid
+          item
+          xs={12}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+          }}
+        >
+          <Box flexGrow={1} />
+          <Box sx={{ mr: "10px" }} className="footer-logo-box">
+            {/* Logo is maintained in CSS */}
+            <div className="footer-logo"></div>
           </Box>
-          <Box className="footer-links-box">
-            <Typography variant="body2" className="company-name">
+          <Box>
+            <Typography variant="body2" sx={{ textAlign: "center" }}>
               <Link href="/terms" color="inherit" underline="hover">
                 ©2023 Sacred Genesis LLC
               </Link>
             </Typography>
-            <Typography variant="body2" className="footer-link-item">
+            <Typography variant="body2" sx={{ textAlign: "center" }}>
               <Link href="/privacy" color="inherit" underline="hover">
                 Privacy Policy
               </Link>
